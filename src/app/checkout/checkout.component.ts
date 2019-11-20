29cc8d861f3e99fc9edc7970e152512226edcadd
Cameron Parus <Cam@Camerons-Mac-Pro.local>
2019-11-20 13:10:16 -0600

    basic styling with stripe elements

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+
+declare var Stripe;
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
-export class CheckoutComponent implements OnInit {
+
+export class CheckoutComponent implements AfterViewInit {
+
+  stripe;
+  card;
+  cardErrors;
+
+  @ViewChild('cardElement', {static: false}) cardElement: ElementRef;
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+
+    console.log(this.cardElement)
+
+
+    this.stripe = Stripe('pk_test_yrwCXVF8CtZM1xR9tp4IEW9700MuZEZs5p');
+    const elements = this.stripe.elements();
+
+    this.card = elements.create('card');
+    this.card.mount(this.cardElement.nativeElement);
+
+    this.card.addEventListener('change', ({ error }) => {
+        this.cardErrors = error && error.message;
+    });
   }
 
 }
